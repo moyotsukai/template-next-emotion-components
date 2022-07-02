96671f3815247e9e569fc96283619a79feeb6dac
Shinchan-git <gyoshu.bowie@gmail.com>
2022-07-02 19:10:11 +0900

    add: Seo

---
+++ components/common/Seo.tsx
@@ -0,0 +1,25 @@
+import Head from 'next/head'
+import React from 'react'
+
+const Seo: React.FC = () => {
+  return (
+    <Head>
+      <title>Shintaro Aoi Portfolio</title>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width,initial-scale=1" />
+      {/*
+      <meta name="description" content="" />
+      <meta property="og:url" content="" />
+      <meta property="og:title" content="" />
+      <meta property="og:description" content="" />
+      <meta property="og:image" content="" />
+      <meta name="twitter:card" content="" />
+      <meta name="twitter:title" content="" />
+      <meta name="twitter:image" content="" />
+      <meta name="theme-color" content="" />
+      */}
+    </Head>
+  )
+}
+
+export default Seo
